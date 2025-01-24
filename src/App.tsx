b1704129a3abd4ae68133340a9298f54cfca544b
Péter Komlósi <komlosi.peti@google.com>
2025-01-24 02:14:11 +0100

    fix: title

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ function App() {
   return (
     <div>
       <div className="mx-auto grid h-screen w-5/6 max-w-[1000px] grid-rows-10 overflow-hidden p-4">
-        <div className="row-span-2">
-          <h1 className="text-center font-serif text-3xl font-bold">
-            Javascript{' '}
+        <div className="row-span-2 text-center">
+          <span className="text-center font-serif text-3xl font-bold">
             <a
               href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math"
               target="__blank"
               title="Math - JavaScript | MDN">
-              math
+              Javascript math
             </a>
             {' '}methods visualized
-          </h1>
+          </span>
+          <small>{' '}by <a href="https://github.com/thavixt/js-math-vis" target="__blank">thavixt @ github</a></small>
           <Controls />
         </div>
         <div className="row-span-10 h-full overflow-auto rounded-md border border-gray-700 text-sm">
